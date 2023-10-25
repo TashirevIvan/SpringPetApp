6159623e319b49b410dad8f23bdd8dbc68354048
TashirevIvan <tashirev.ivan@yandex.ru>
2023-10-26 00:08:06 +0300

    done

---
+++ src/main/java/ru/appline/logic/PetModel.java
@@ -0,0 +1,43 @@
+package ru.appline.logic;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+public class PetModel implements Serializable {
+
+    private static final PetModel instance = new PetModel();
+
+    private final Map<Integer, Pet> model;
+
+    public PetModel() {
+        model = new HashMap<Integer, Pet>();
+    }
+
+    public static PetModel getInstance() {
+        return instance;
+    }
+
+    public void add(Pet pet, int id) {
+        model.put(id, pet);
+    }
+
+    public Pet getFromList(int id) {
+        return model.get(id);
+    }
+
+    public Map<Integer, Pet> getAll() {
+        return model;
+    }
+
+    public void delete(int id) {
+        model.remove(id);
+    }
+
+    public void editPetList(Map<String, Pet> editedPetMap) {
+        for (Map.Entry<String, Pet> petEntry : editedPetMap.entrySet()) {
+            Integer petId = Integer.parseInt(petEntry.getKey());
+            model.replace(petId, petEntry.getValue());
+        }
+    }
+}
